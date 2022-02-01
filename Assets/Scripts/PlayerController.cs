fce4bb2b6f35220ea818c169b5ba739337a90e06
Daviti Bujiashvili <davit.bujiashvili@geolab.edu.ge>
2022-02-01 18:32:16 +0400

    add Player Animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,7 @@ public class PlayerController : MonoBehaviour
     public float MoveSpeed;
     public float JumpSpeed;
 
-    public bool IsJumping;
-
+    private Animator animator;
     public bool IsGrounded;
     public Transform JumpPoint;
     public float radius;
@@ -18,6 +17,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
+        animator=GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,7 +26,7 @@ public class PlayerController : MonoBehaviour
         IsGrounded=Physics2D.OverlapCircle(JumpPoint.position,radius,Mask);
 
         if(Input.GetAxisRaw("Horizontal")>0){
-            rb.velocity=new Vector3(MoveSpeed,rb.velocity.y,0f);
+            rb.velocity=new Vector3(MoveSpeed,rb.velocity.y,0f); 
         }
 
         if(Input.GetAxisRaw("Horizontal")<0){
@@ -40,6 +40,8 @@ public class PlayerController : MonoBehaviour
         if(Input.GetButtonDown("Jump") && IsGrounded){
             rb.velocity=new Vector3(0f,JumpSpeed,0f);
         }
+        animator.SetInteger("speed",(int)Mathf.Abs(rb.velocity.x));
+        animator.SetBool("isGrounded",IsGrounded);
         
     }
 }
